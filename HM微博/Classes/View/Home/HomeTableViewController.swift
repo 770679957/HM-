59cc019280fa50b3a0005574bde103c31795cd5e
hongmei <hongmei@hongmeidemac-mini.local>
2019-04-25 00:20:14 +0800

    第9章 9.1.5 显示微博数据

--- HM微博/Classes/View/Home/HomeTableViewController.swift
+++ HM微博/Classes/View/Home/HomeTableViewController.swift
@@ -7,8 +7,20 @@
 //
 
 import UIKit
+import SVProgressHUD
+
+
+/// 原创微博 Cell 的可重用表示符号
+ private let StatusCellNormalId = "StatusCellNormalId"
+/// 转发微博 Cell 的可重用标识符号
+ private let StatusCellRetweetedId = "StatusCellRetweetedId"
 
 class HomeTableViewController: VisitorTableViewController {
+    //微博数据数组
+    private lazy var listViewModel = StatusListViewModel()
+
+    //微博数据数组
+    var dataList = [Status]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,52 +30,71 @@ class HomeTableViewController: VisitorTableViewController {
             return
         }
         loadData()
+        prepareTableView()
         
     }
-
-    
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    //准备表格
+    private func prepareTableView() {
+        //注册可重用cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: StatusCellNormalId)
         
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
     }
     
     //加载数据
     private func loadData() {
-        NetworkTools.sharedTools.loadStatus { (result,error) -> () in
-            if error != nil {
-                print("出错了")
-                return
+//        NetworkTools.sharedTools.loadStatus { (result,error) -> () in
+//            if error != nil {
+//                print("出错了")
+//                return
+//
+//            }
+//            //print(result)
+//            //判断result的数据结构是否正确
+//            let result1 = result as? [String:AnyObject]
+//            guard let array = result1?["statuses"] as? [[String:AnyObject]]
+//                else {
+//                    print("数据格式错误")
+//                    return
+//            }
+//           // print(array)
+//            //遍历数组
+//            var dataList = [Status]()
+//
+//            for dict in array {
+//                dataList.append(Status(dict: dict))
+//            }
+//            //测试
+//            print(dataList)
+//            self.dataList = dataList
+//            //刷新数据
+//            self.tableView.reloadData()
+//        }
+        
+        listViewModel.loadStatus{ (isSuccessed) -> () in
+            if !isSuccessed {
                 
+                SVProgressHUD.showInfo(withStatus: "加载数据错误，请稍后再试")
+                return
             }
-            print(result)
-            //判断result的数据结构是否正确
-            let result1 = result as? [String:AnyObject]
-            guard let array = result1?["statuses"] as? [[String:AnyObject]]
-                else {
-                    print("数据格式错误")
-                    return
-            }
-           // print(array)
-            //遍历数组
-            var dataList = [Status]()
-            
-            for dict in array {
-                dataList.append(Status(dict: dict))
-            }
-            //测试
-            print(dataList)
+            print(self.listViewModel.statusList)
             //刷新数据
             self.tableView.reloadData()
         }
         
     }
+}
 
+//数据源方法
+extension HomeTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listViewModel.statusList.count
+    }
     
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: StatusCellNormalId, for: indexPath)
+        //测试微博信息内容
+        cell.textLabel?.text = listViewModel.statusList[indexPath.row].text
+        return cell
+    }
+    
 }
